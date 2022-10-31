5947fdb37a1b13c27a82b54de01e141423d6af1f
ICodeMyOwnLife <icodemyownlife@gmail.com>
2022-10-31 10:45:17 +0700

    Add

--- packages/test-web/src/index.ts
+++ packages/test-web/src/index.ts
@@ -1,12 +1,22 @@
 import { copy } from '@web/rollup-plugin-copy';
-import { readdirSync } from 'node:fs';
+import { sync } from 'fast-glob';
+import { parse } from 'node:path';
 import { defineConfig } from 'rollup';
 import ts from 'rollup-plugin-ts';
 
-const config = defineConfig({
-  input: readdirSync('./src').map(file => `./src/${file}`),
+const SRC_DIR = './src/';
+
+const entries = Object.fromEntries(
+  sync([`${SRC_DIR}**/*.{ts,tsx}`]).map(fileName => {
+    const { dir, name, base } = parse(fileName);
+    return [`${dir}/${name}`.slice(SRC_DIR.length), `${dir}/${base}`];
+  }),
+);
+
+export const config = defineConfig({
+  input: entries,
   output: { dir: 'dist', format: 'esm' },
   plugins: [ts(), copy({ exclude: [], patterns: ['**/*'], rootDir: 'public' })],
 });
 
-export default config;
+export default [config];
