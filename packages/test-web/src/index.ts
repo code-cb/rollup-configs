4f76ea07dc00a1442689e1bca6082af2f7967dde
ICodeMyOwnLife <icodemyownlife@gmail.com>
2022-11-01 15:40:19 +0700

    update

--- packages/test-web/src/index.ts
+++ packages/test-web/src/index.ts
@@ -7,7 +7,7 @@ import ts from 'rollup-plugin-ts';
 const SRC_DIR = './src/';
 
 const entries = Object.fromEntries(
-  sync([`${SRC_DIR}**/*.{ts,tsx}`]).map(fileName => {
+  sync([`${SRC_DIR}**/*.{ts,tsx}`], { stats: false }).map(fileName => {
     const { dir, name, base } = parse(fileName);
     return [`${dir}/${name}`.slice(SRC_DIR.length), `${dir}/${base}`];
   }),
